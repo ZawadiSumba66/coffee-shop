3b953c4154724c11267c9f25669634dad9cb9407
ZawadiSumba66 <rosesumba98@gmail.com>
2022-02-22 16:02:10 +0300

    Create coffee data and component

---
+++ src/containers/Dashboard/CoffeeData.ts
@@ -0,0 +1,92 @@
+const Popular = [
+  {
+    image: 'https://images.unsplash.com/photo-1594261956806-3ad03785c9b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcHB1Y2lub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Capuccino',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1503481766315-7a586b20f66d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Latte',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1510707577719-ae7c14805e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGVzcHJlc3NvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Espresso',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1544681369-310c49478a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Americano',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Caffe Mocha',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1600352090511-0f801b8f3d67?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Machiatto',
+  },
+  {
+    image: '',
+    name: '',
+  },
+];
+
+const Latte = [
+  {
+    image: 'https://image.shutterstock.com/image-photo/coconut-latte-on-wooden-table-600w-536543152.jpg',
+    name: 'Coconut Latte',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/festive-hot-chocolate-whipped-cream-600w-1176402949.jpg',
+    name: 'Hazelnut Latte',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/hot-almond-caramel-macchiato-600w-1345916870.jpg',
+    name: 'Caramel Machiatto Latte',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/masala-pulled-tea-chai-latte-600w-520223158.jpg',
+    name: 'Cinammon Dolce Latte',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/cup-freshly-made-delicious-cappuccino-600w-1903123363.jpg',
+    name: 'Caffe Latte',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/coffee-mocha-on-wood-desk-600w-374380330.jpg',
+    name: 'Caffe Mocha',
+  },
+];
+
+const Espresso = [
+  {
+    image: 'https://images.unsplash.com/photo-1522922235461-912955b0772b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmlzdHJldHRvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Ristretto',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1630021439100-74a32ab42d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDExfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Doppio',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1594261956806-3ad03785c9b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcHB1Y2lub3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Capuccino',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/ice-mocha-coffee-600w-1374608741.jpg',
+    name: 'Mocha',
+  },
+  {
+    image: 'https://image.shutterstock.com/image-photo/breakfast-cafe-useful-drink-dessert-600w-564073720.jpg',
+    name: 'Espresso Machiatto',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1544681369-310c49478a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Americano',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1510707577719-ae7c14805e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGVzcHJlc3NvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+    name: 'Espresso',
+  },
+];
+
+const CoffeeData = [Popular, Latte, Espresso];
+
+export default CoffeeData;
---
+++ src/containers/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Dashboard.css';
+
+/* eslint-disable react/jsx-filename-extension */
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <p>dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
