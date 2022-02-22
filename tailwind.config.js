0fe46e5beff0a38aabd81f9821ea8cba5e0c46cb
ZawadiSumba66 <rosesumba98@gmail.com>
2022-02-22 11:13:39 +0300

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./assets/cofee-background.jpg')",
+      }
+    },
   },
   plugins: [],
 }
