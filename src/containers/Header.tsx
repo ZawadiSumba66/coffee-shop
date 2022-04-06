75bffc5ffb67cd447c07b7d160feaa263a47c6f7
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:53:14 +0300

    Add an avatar element for display

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Avatar } from '@mui/material';
+import { connect } from 'react-redux';
 import { Dropdown } from 'react-bootstrap';
-import { Link } from '@reach/router';
+import { Link } from 'react-router-dom';
 import Logo from './Logo';
+import store from '../redux/store';
+import { fetchAvatar } from '../redux/slices/avatar.slice';
 
-function Header() {
+type HeaderProps = {
+  avatar: any
+};
+
+function Header({ avatar }: HeaderProps) {
+  useEffect(() => {
+    store.dispatch(fetchAvatar());
+  }, []);
+
   const logOut = () => localStorage.clear();
   return (
     <div className="flex justify-between">
       <Logo />
       <Dropdown>
         <Dropdown.Toggle className="flex items-center bg-gray-700 hover:bg-gray-600 focus:bg-gray-500 border-gray-700">
-          <Avatar src="/broken-image.jpg" className="cursor-pointer mr-3" />
+          <Avatar src={avatar.avatar_url} className="cursor-pointer mr-3" />
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
           <Dropdown.Item className="active:bg-gray-300"><Link to="/dashboard" className="hover:no-underline">Dashboard</Link></Dropdown.Item>
-          <Dropdown.Item className="active:bg-gray-300"><Link to="/favorites" className="hover:no-underline">My favorites</Link></Dropdown.Item>
+          <Dropdown.Item className="active:bg-gray-300"><Link to="/orders" className="hover:no-underline">My Orders</Link></Dropdown.Item>
           <Dropdown.Item className="active:bg-gray-300"><Link to="/settings" className="hover:no-underline">Settings</Link></Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item className="active:bg-gray-300"><Link to="/login" onClick={logOut} className="hover:no-underline">Log Out</Link></Dropdown.Item>
@@ -26,4 +37,14 @@ function Header() {
   );
 }
 
-export default Header;
+type AvatarState = {
+  avatar: {
+    avatar: any,
+  }
+};
+
+const mapStateToProps = (state: AvatarState) => ({
+  avatar: state.avatar.avatar,
+});
+
+export default connect(mapStateToProps)(Header);
