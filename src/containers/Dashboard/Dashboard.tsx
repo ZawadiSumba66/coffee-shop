02f20c2910ca4402eaf5782bf564b0e396e05262
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:52:12 +0300

    Add fetch user method to display user name on the dashboard header

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+// import { Spinner } from 'react-bootstrap';
 import Header from '../Header';
 import Footer from '../Footer';
 import Coffees from './Coffees';
+import { UserData, fetchUser } from '../../redux/slices/user.slice';
 import './Dashboard.css';
+import store from '../../redux/store';
 
-function Dashboard() {
+type DashboardProps = {
+  user: UserData
+};
+
+function Dashboard({ user }: DashboardProps) {
+  const token = localStorage.getItem('token');
+  useEffect(() => {
+    store.dispatch(fetchUser());
+  }, []);
+
+  if (!user) {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <p>loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="h-auto">
       <div className="bg-gradient-to-r from-black bg-amber-700 p-4 h-46 text-white">
         <Header />
         <h1 className="text-2xl font-bold pt-3 pl-10">
           Hey there,
-          <span> Customer!</span>
+          {' '}
+          <span>{`${token ? user.firstname : 'Customer'}`}</span>
         </h1>
         <span className="text-sm italic pl-10">Let us spice up your day with some great coffee, shall we?</span>
       </div>
@@ -25,4 +47,14 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+type UserState = {
+  user: {
+    user: UserData
+  }
+};
+
+const mapStateToProps = (state: UserState) => ({
+  user: state.user.user,
+});
+
+export default connect(mapStateToProps)(Dashboard);
