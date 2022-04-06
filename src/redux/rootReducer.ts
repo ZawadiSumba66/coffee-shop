340b95689d8b791c7abe0c70becd7a02cc167e2a
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:56:55 +0300

    Add coffee slice reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import userReducer from './slices/user.slice';
 import avatarReducer from './slices/avatar.slice';
-import coffeeDataReducer from './slices/coffeedata.slice';
+import categoryDataReducer from './slices/categories.slice';
+import coffeeReducer from './slices/coffee.slice';
 
 const rootReducer = combineReducers({
   user: userReducer,
   avatar: avatarReducer,
-  data: coffeeDataReducer,
+  data: categoryDataReducer,
+  coffee: coffeeReducer,
 });
 
 export default rootReducer;
