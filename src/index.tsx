79d78df58f14ed90a80f844970a9bd51a8976ac0
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:55:18 +0300

    Add a flash and CustomRouter provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import CustomRouter from './routes/CustomRouter';
 import './index.css';
 import store from './redux/store';
-/* eslint-disable react/jsx-filename-extension */
+import Bus from './redux/helpers/bus';
+import history from './routes/history';
 
+declare global {
+  interface Window {
+    flash?: any;
+  }
+}
+
+window.flash = (message: string, type = 'success') => Bus.emit('flash', ({ message, type }));
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <CustomRouter history={history}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </CustomRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
