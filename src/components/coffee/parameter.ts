e63f21476ef62c0cd655394e0a025ee03f0114a6
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:59:29 +0300

    A type file for the coffee parameters

---
+++ src/components/coffee/parameter.ts
@@ -0,0 +1,12 @@
+export type Size = {
+  measure: string,
+  description: string
+};
+
+export type Milk = {
+  type: 'Normal' | 'Soya Milk' | 'Oat Milk',
+};
+
+export type Topping = {
+  type: 'Almond' | 'Caramel' | 'Chocolate' | 'Mint' | 'Vodka',
+};
