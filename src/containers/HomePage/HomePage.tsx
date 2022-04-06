d1e7afaf67b8750565bd6f6453f4e4a85f3138c9
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-06 15:54:15 +0300

    Modify link element import module

--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from '@reach/router';
+import { Link } from 'react-router-dom';
 import { faMugHot, faCoffee, faBeer } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Logo from '../Logo';
